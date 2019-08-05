8d0fa41ff50204abd8bc55033134cbb2742adb68
Dhairya Punjabi <dhairyapunjabi49@gmail.com>
2019-08-05 14:36:31 +0530

    Create fillable items in User model

--- app/User.php
+++ app/User.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     //
+    protected $fillable = [
+      'name',
+      'designation',
+      'email_id',
+      'date_of_birth',
+      'gender',
+      'contact_number'
+    ];
 }
