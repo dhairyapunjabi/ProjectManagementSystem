fceab6341d62ce2f9c343c5d83eff3b18a373477
Dhairya Punjabi <dhairyapunjabi49@gmail.com>
2019-08-05 14:23:21 +0530

    Create necessary fields in User model migration

--- database/migrations/2019_08_05_083406_create_users_table.php
+++ database/migrations/2019_08_05_083406_create_users_table.php
@@ -6,26 +6,32 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('users', function (Blueprint $table) {
+      $table->bigIncrements('id');
+      $table->string('name');
+      $table->string('designation');
+      $table->string('email_id');
+      $table->date('date_of_birth');
+      $table->string('gender');
+      $table->integer('contact_number')->unsigned();
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('users');
+  }
 }
